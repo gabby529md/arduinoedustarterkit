415433b6711cf1673aa66f630695aa8b09f10e9c
Gabby D <gabby529md@gmail.com>
2025-08-19 09:19:18 +0100

    Lesson 6 - Sports Robot
    Lesson 6 - Sports Robot

---
+++ Lesson6_SportsRobot/Lesson6_SportsRobot.ino
@@ -0,0 +1,16 @@
+#include <Servo.h>
+Servo myServo;
+const int potPin = A0;
+int potValue = 0;
+int servoAngle = 0;
+
+void setup() {
+  myServo.attach(9); 
+}
+
+void loop() {
+  potValue = analogRead(potPin);
+  servoAngle = map(potValue, 0, 1023, 0, 179);
+  myServo.write(servoAngle);
+  delay(100);
+}
