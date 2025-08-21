29a9a92304686180c6c23b6ea5919a9568e9b377
Gabby D <gabby529md@gmail.com>
2025-08-21 05:44:58 +0100

    Lesson 6a - Sports Robot - Push Button
    Lesson 6a - Sports Robot - Push Button

---
+++ Lesson6_SportsRobot_PushButton/Lesson6_SportsRobot_PushButton.ino
@@ -0,0 +1,30 @@
+#include <Servo.h>
+Servo myServo;
+const int potPin = A0;
+const int buttonPin = 7;
+int potValue = 0;
+int servoAngle = 0;
+
+void setup() {
+  myServo.attach(9);
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  if (digitalRead(buttonPin) == HIGH) {
+    myServo.write(179);
+  }
+  else {
+    potValue = analogRead(potPin);
+    servoAngle = map(potValue, 0, 1023, 0, 179);
+    myServo.write(servoAngle);
+  }
+  
+  Serial.print("potValue: ");
+  Serial.print(potValue);
+  Serial.print(", servoAngle: ");
+  Serial.println(servoAngle);
+
+  delay(1000);
+}
